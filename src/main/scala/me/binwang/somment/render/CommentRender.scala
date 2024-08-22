0ed9757300c2a6091d69b38d27f032939ff9049e
Bin Wang <bin.wang@mail.binwang.me>
2024-08-22 16:37:27 -0400

    better comment style

--- src/main/scala/me/binwang/somment/render/CommentRender.scala
+++ src/main/scala/me/binwang/somment/render/CommentRender.scala
@@ -6,6 +6,17 @@ import scalatags.JsDom.all.*
 
 object CommentRender {
 
+  private val indentColors = Seq(
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "purple",
+    "rebeccapurple",
+  )
+
   def apply(comments: Seq[Comment]): Node = {
     renderComments(comments, 0).render
   }
@@ -26,16 +37,22 @@ object CommentRender {
 
   private def renderComment(comment: Comment, depth: Int): Frag = {
     val childrenClass = if (depth > 0) "comment-child" else ""
+    val indentColor = if (depth > 0) Some(indentColors((depth-1) % indentColors.size)) else None
     div(
       cls := s"comment $childrenClass",
       div(
-        cls := "comment-author",
-        comment.author,
+        cls := "comment-self",
+        indentColor.map(c => borderLeftColor := c),
+        div(
+          cls := "comment-author",
+          comment.author,
+        ),
+        div(
+          cls := "comment-content",
+          comment.text,
+        ),
       ),
-      div(
-        cls := "comment-content",
-        comment.text,
-      ),
+      hr(cls := "comment-separator"),
       renderComments(comment.children, depth + 1),
     )
   }
