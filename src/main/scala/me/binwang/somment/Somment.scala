85d9b1b440217aff2fd72fb9e515cf570642f684
Bin Wang <bin.wang@mail.binwang.me>
2025-05-22 10:51:30 -0400

    add progress bar and error message

--- src/main/scala/me/binwang/somment/Somment.scala
+++ src/main/scala/me/binwang/somment/Somment.scala
@@ -8,6 +8,8 @@ import scala.scalajs.js.annotation.JSExportTopLevel
 import cats.effect.unsafe.implicits.global
 import me.binwang.somment.render.CommentRender
 import org.scalajs.dom.Element
+import scalatags.JsDom.all.*
+import scalatags.JsDom.tags2.progress
 
 @JSExportTopLevel("Somment", moduleID = "somment")
 class Somment(url: String, elem: Element) extends js.Object {
@@ -16,13 +18,19 @@ class Somment(url: String, elem: Element) extends js.Object {
     new RedditFetcher(),
     new HackerNewsFetcher(),
   )
-  
+
   def create(): Unit = {
-    fetchers.find(_.canHandle(url)).foreach { fetcher =>
+    val progressBar = progress(id := "comment-progress").render
+    elem.replaceWith(progressBar)
+    fetchers.find(_.canHandle(url)).map { fetcher =>
       fetcher.getComments(url).map { comments =>
-        elem.replaceWith(CommentRender(comments))
-      }.unsafeRunAndForget()     
-    }
+        progressBar.replaceWith(CommentRender(comments))
+      }.handleError{ err =>
+        progressBar.replaceWith(div(id := "comment-error", s"Error to load comment: ${err.getMessage}").render)
+      }.unsafeRunAndForget()
+    }.getOrElse(
+      progressBar.replaceWith(div(cls := "comment", "No comment to load").render)
+    )
   }
 
 }
